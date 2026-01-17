d2bdb112a7b262cb6f4bfa9fcc3444f4fa99fcb7
Vivek Yadav <vivek.yadav.802056@gmail.com>
2026-01-17 23:40:08 +0530

    modify the income class

--- src/model/Income.java
+++ src/model/Income.java
@@ -3,6 +3,11 @@ package model;
 import java.io.Serializable;
 import java.time.LocalDate;
 
+// The
+//Serializable interface in Java is a marker interface that signals to the Java Virtual Machine (JVM)
+// that objects of that class can be converted into a byte stream and back.
+// This process, called serialization, is used to save an object's state so it can be stored or transferred
+// and later reconstructed (deserialized).
 public class Income implements Serializable {
 
     private static final long serialVersionUID = 1L;
