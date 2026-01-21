f6bb06d525f3afa117ef705ce96ad80177cddad1
Vivek Yadav <vivek.yadav.802056@gmail.com>
2026-01-21 23:35:29 +0530

    update the expense manager

--- src/service/ExpenseService.java
+++ src/service/ExpenseService.java
@@ -2,16 +2,27 @@ package service;
 
 import model.Expense;
 
-import java.util.ArrayList;
 import java.util.List;
-// Expense service
+
 public class ExpenseService {
 
-    private List<Expense> expenses = new ArrayList<>();
+    private static final String EXPENSE_FILE = "data/expenses.dat";
+
+    private List<Expense> expenses;
     private int expenseIdCounter = 1;
+
+    public ExpenseService() {
+        expenses = FileService.loadFromFile(EXPENSE_FILE);
+
+        if (!expenses.isEmpty()) {
+            expenseIdCounter =
+                    expenses.get(expenses.size() - 1).getId() + 1;
+        }
+    }
 
     public void addExpense(Expense expense) {
         expenses.add(expense);
+        FileService.saveToFile(expenses, EXPENSE_FILE);
     }
 
     public List<Expense> getAllExpenses() {
